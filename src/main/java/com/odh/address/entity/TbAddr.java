379bf8e7e1be6a88500caefc219919b730cb2ffb
ODH <odh4145@gmail.com>
2022-11-08 00:00:32 +0900

    221107 주석달기

--- src/main/java/com/odh/address/entity/TbAddr.java
+++ src/main/java/com/odh/address/entity/TbAddr.java
@@ -13,14 +13,17 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class TbAddr {
 
+    // pk 값
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment 처리
     @Column(name = "id", nullable = false)
     private Long id;
 
+    // 이름
     @Column(name = "name", nullable = false, length = 100)
     private String name;
 
+    // 주소
     @Column(name = "addr", nullable = false, length = 100)
     private String addr;
 
